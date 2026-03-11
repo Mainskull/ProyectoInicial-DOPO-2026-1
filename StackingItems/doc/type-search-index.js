daac9ebe9b50b21987dc5bcbafc59d7e1fba58dd
Juan Nieto <ing.sistemas3.14@gmail.com>
2026-03-10 23:19:44 -0500

    actualizacion de archivos del proyecto

---
+++ StackingItems/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Canvas"},{"p":"<Unnamed>","l":"Circle"},{"p":"<Unnamed>","l":"Cup"},{"p":"<Unnamed>","l":"Lid"},{"p":"<Unnamed>","l":"Rectangle"},{"p":"<Unnamed>","l":"Triangle"}];updateSearchResults();
